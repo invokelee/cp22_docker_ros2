54070899bb58d0ae547f3cd88b1d09dde554f859
Sanghoon Lee <invokelee@gmail.com>
2024-08-14 14:29:38 +0000

    Complete Task4 ROS2 Real Robot docker compose and testing #1

---
+++ tortoisebot_ros2_docker/tortoisebot-ros2-slam-real/entrypoint.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+echo "[$(date +'%F %T')] Starting the ROS2 Mapping for Real tortoisebot..."
+hostname
+cat /etc/hosts
+echo $ROS_MASTER_URI
+source /tortoise_ws/install/setup.bash \
+&& ros2 launch tortoisebot_slam cartographer.launch.py use_sim_time:=False
