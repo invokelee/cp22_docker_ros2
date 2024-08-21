d976a17857767d5d94f603b08d131da00d5247a9
Sanghoon Lee <invokelee@gmail.com>
2024-08-21 12:09:03 +0000

    Task4 Real2-Change entrypoint.sh for ros2 daemon start on the container and testing #3

--- tortoisebot_ros2_docker/tortoisebot-ros2-real/entrypoint.sh
+++ tortoisebot_ros2_docker/tortoisebot-ros2-real/entrypoint.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 echo "[$(date +'%F %T')] Starting ROS2 Real Tortoisebot firmware bringup..."
+ros2 daemon stop
+ros2 topic list
 source /tortoise_ws/install/setup.bash \
 && ros2 launch tortoisebot_bringup bringup.launch.py use_sim_time:=False
